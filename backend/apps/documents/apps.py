e127ab3c25ae4c07da41983e69a2ee454ff7d3be
GreggWolin <GreggWolin.wolin@crescentbayholdings.com>
2026-01-23 16:05:40 -0700

    Remove deferred pdfplumber import from DocumentsConfig

--- backend/apps/documents/apps.py
+++ backend/apps/documents/apps.py
@@ -4,10 +4,3 @@ from django.apps import AppConfig
 class DocumentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.documents"
-
-    def ready(self):
-        try:
-            # deferred heavy/native imports go here
-            import pdfplumber
-        except ImportError as e:
-            pass
